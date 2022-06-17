c491fbaea7eae703deadc37d89f59ae59a3aec1c
Yuchen <smirkiiiv@gmail.com>
2022-06-17 09:21:51 +0800

    Initial Commit

---
+++ musicPlayerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicPlayerApp
+//
+//  Created by 林祐辰 on 2022/6/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
